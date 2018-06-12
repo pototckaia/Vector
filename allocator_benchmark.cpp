cc326270c46b78d864753c91a7f9ba40ba0ded74
pototckaia <pototckaia@gmail.com>
2018-06-12 22:32:42 +1000

    add string benchmark

--- allocator_benchmark.cpp
+++ allocator_benchmark.cpp
@@ -4,42 +4,91 @@
 #include <list>
 
 #include <benchmark/benchmark.h>
+#include <tgmath.h>
+#include <random>
 #include "allocator.h"
 
+
+
 template <typename Allocator>
-void BM_fixed_allocator(benchmark::State& state) {
-    std::list<int, Allocator> test;
-    int limit = 10000000;
-    for(int i = 0; i < limit; ++i) {
-        test.push_back(i);
-    }
+void BM_fixed_allocator_int(benchmark::State& state) {
 
-    test.erase(test.begin());
-    test.erase(test.end());
+    for (auto _: state) {
 
-    auto iter = test.begin();
-    for (int i = 0; i < 1000; i++) { iter++; }
-    test.erase(iter);
+        std::list<int, Allocator> test;
+        int limit = 1000000;
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(i);
+        }
 
-    for(int i = 0; i < limit; ++i) {
-        test.push_back(i);
-    }
+        test.erase(test.begin());
+        test.erase(std::prev(test.end()));
 
-    iter = test.begin();
-    for (int i = 0; i < 8; ++i) {
-        for (int i = 0; i < 1000; i++) { iter++; }
+        auto iter = test.begin();
+        std::advance(iter, 1000);
         test.erase(iter);
-    }
 
-    for(int i = 0; i < limit; ++i) {
-        test.push_back(i);
-    }
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(i);
+        }
 
-    test.clear();
+        iter = test.begin();
+        for (int i = 0; i < 8; ++i) {
+            std::advance(iter, 1000);
+            iter = test.erase(iter);
+        }
+
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(i);
+        }
+
+        test.clear();
+    }
+}
+
+template <typename Allocator>
+void BM_fixed_allocator_string(benchmark::State& state) {
+
+    for (auto _: state) {
+
+        std::list<std::string, Allocator> test;
+        int limit = 100000;
+        std::string s(100, ' ');
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(s);
+        }
+
+        test.erase(test.begin());
+        test.erase(std::prev(test.end()));
+
+        auto iter = test.begin();
+        std::advance(iter, 1000);
+        test.erase(iter);
+
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(s);
+        }
+
+        iter = test.begin();
+        for (int i = 0; i < 8; ++i) {
+            std::advance(iter, 1000);
+            iter = test.erase(iter);
+        }
+
+        for(int i = 0; i < limit; ++i) {
+            test.push_back(s);
+        }
+
+        test.clear();
+    }
 }
 
 
-BENCHMARK_TEMPLATE(BM_fixed_allocator, std::allocator<int>);
-BENCHMARK_TEMPLATE(BM_fixed_allocator, kkk::Allocator<int>);
+
+BENCHMARK_TEMPLATE(BM_fixed_allocator_int, std::allocator<int>);
+BENCHMARK_TEMPLATE(BM_fixed_allocator_int, kkk::Allocator<int>);
+
+BENCHMARK_TEMPLATE(BM_fixed_allocator_string, std::allocator<std::string>);
+BENCHMARK_TEMPLATE(BM_fixed_allocator_string, kkk::Allocator<std::string>);
 
 BENCHMARK_MAIN();
